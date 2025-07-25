395bb30af091565be0882fc79deff1de61a01b04
Ian-Protocol <ipascal@live.ca>
2025-07-24 23:10:40 -0500

    Add pages and about page

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get "pages/about"
   get "cards/index"
   get "cards/show"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
